c3336d6332b5c06f76cfd49cc6fa0b13698b23bd
Furkan Uyar <furkanuyar@Cihans-MacBook-Air.local>
2020-06-28 14:39:23 +0300

    Update secret key with env variable

--- sd_site/settings.py
+++ sd_site/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0!u)y*d($4o9h-8!ppd5a-vz05p541s4ap281q76ls)6v1w)x9'
+SECRET_KEY = os.environ['DICTIONARY_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
